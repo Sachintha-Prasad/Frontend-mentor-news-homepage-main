968179161cd4e438c9d16c0dc319eacab946ae64
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-01 17:21:32 +0530

    build

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import notFoundImg from "../assets/images/not-found.svg"
 const NotFoundPage = () => {
     return (
         <div className="container flex flex-col items-center my-16">
-            <img src={notFoundImg} alt="" className="w-full max-w-[450px]" />
+            <img src={notFoundImg} alt="" className="w-full max-w-[450px]`" />
 
             <h2 className="text-soft-red text-5xl lg:text-7xl text-center font-bold">
                 404
